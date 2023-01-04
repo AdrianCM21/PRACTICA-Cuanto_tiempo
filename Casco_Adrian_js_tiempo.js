26fbffea1522331417e19b114d0d3f9f0fe92d56
AdrianCM21 <113633896+AdrianCM21@users.noreply.github.com>
2023-01-04 20:54:20 -0300

    Add files via upload

---
+++ Casco_Adrian_js_tiempo.js
@@ -0,0 +1,63 @@
+Number.prototype.isPrime = function() {
+        for( let i=2; i< Math.sqrt(this); i++ ) {
+            if( this % i === 0 ) {    
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    const { performance } = require('perf_hooks');
+    const start = performance.now();
+    let primeCount = 0;
+    let num = 2; // por razones matemáticas, 1 se considera primo
+    while( primeCount < 1e5 ) {
+        if( num.isPrime() ) {
+            primeCount++;
+        }
+        num++;
+    }
+    console.log(`The 10,000th prime number is ${num-1}`);
+    console.log(`This took ${performance.now() - start} milliseconds to run`);
+
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+//Fibonacci
+//¿Qué implementación de Fibonacci debería ser más rápida?
+// recursive
+function rFib( n ) {
+    if ( n < 2 ) {
+        return n;
+    }
+    return rFib( n-1 ) + rFib( n-2 );
+}
+rFib(20);
+    // iterative
+function iFib( n ) {
+    const vals = [ 0, 1 ];
+    while(vals.length-1 < n) {
+        let len = vals.length;
+            vals.push( vals[len-1] + vals[len-2] );
+        }
+    return vals[n];
+}
+iFib(40);
+//RESPUESTA: La forma mas rapidad es (iterative)
+
+
+
+/*---------------------------------------------------------------------------------------------------------------------*/
+//mas eficiente
+const reverse=(story)=>{
+    let aux='';
+    for (let index = 0; index <story.length; index++) {
+        aux = story[index]+aux;
+    }
+    return aux;
+}
+const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+const reversed1 = story.split("").reverse().join("");
+const reversed2=reverse(story);
+console.log(reversed1);
+console.log(reversed2);
+// Se puede realizar una funcion que invierte el contenido de un string o array mas varsatil y entendible
